097257f74b06a55e95a52a5eb776bef33cd1c797
Ali Zand <ali.zand@realpage.com>
2021-08-26 15:00:06 -0700

    Add pdf

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,7 +1,16 @@
+import { jsPDF } from 'jspdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+
 const Preview = () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const doc = new jsPDF();
+  doc.table(10, 10, [{ '#': '1', 'Description': 'Hello World' }], ['#', 'Description'], { autoSize: true })
+
   return (
     <div>
-      Preview
+      <Document file={doc.output('bloburi')}>
+        <Page pageNumber={1} />
+      </Document>
     </div>
   )
 };
