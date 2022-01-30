fed41d9350303909fcc50d6ca033eabe0ed13705
Ali Zand <ali.zand9224@gmail.com>
2022-01-30 08:57:59 -0800

    Update bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+
 import InvoicePage from './components/InvoicePage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<InvoicePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="invoice" element={<InvoicePage />} />
     </Routes>
   );
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+
+const HomePage = () => {
+  return (
+    <Container fluid={true}>
+      <div>
+        test
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
